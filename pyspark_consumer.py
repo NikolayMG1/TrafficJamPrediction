bd6932025a41a3f8cd2627cc879845c43d23680e
Rali <ralica.a.simova@gmail.com>
2026-01-10 12:56:22 +0200

    Read from Kafka in pyspark

--- pyspark_consumer.py
+++ pyspark_consumer.py
@@ -2,38 +2,36 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, avg, count
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
 
-# 1. Start Spark session
-spark = SparkSession.builder \
-    .appName("DigitrafficRealTime") \
-    .getOrCreate()
+from kafka_producer import KAFKA_TOPIC
+
+spark = SparkSession.builder.appName("DigitrafficRealTime").getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# 2. Define schema for Digitraffic station data
-schema = StructType([
-    StructField("id", IntegerType(), True),
-    StructField("name", StringType(), True),
-    StructField("avgSpeed", DoubleType(), True),
-    StructField("vehicleCount", IntegerType(), True),
-    StructField("direction", IntegerType(), True),
-    StructField("latitude", DoubleType(), True),
-    StructField("longitude", DoubleType(), True)
-    # Add more fields if needed
+sensor_event_schema = StructType([
+    StructField("event_time", StringType(), True),     
+    StructField("station_id", IntegerType(), True),
+
+    StructField("entry_id", IntegerType(), True),
+    StructField("station_id_sensor_value", IntegerType(), True),
+
+    StructField("sensor_name", StringType(), True),
+    StructField("sensor_short_name", StringType(), True),
+
+    StructField("unit", StringType(), True),
+    StructField("value", DoubleType(), True)
 ])
 
-# 3. Read from Kafka topic
 df_raw = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
-    .option("subscribe", "traffic-digitraffic") \
+    .option("subscribe", KAFKA_TOPIC) \
     .option("startingOffsets", "latest") \
     .load()
 
-# Kafka message value is in bytes, convert to string
 df_str = df_raw.selectExpr("CAST(value AS STRING) as json_str")
 
-# Parse JSON using schema
-df_parsed = df_str.select(from_json(col("json_str"), schema).alias("data")).select("data.*")
+df_parsed = df_str.select(from_json(col("json_str"), sensor_event_schema).alias("data")).select("data.*")
 
 # 4. Compute real-time metrics: average speed per station
 df_metrics = df_parsed.groupBy("id", "name") \
