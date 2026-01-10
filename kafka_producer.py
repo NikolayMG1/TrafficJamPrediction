dd48559f7af1e38784aeb907bd770e8c4fb76b40
Nikolay Georgiev <nikoalisso1253@gmail.com>
2026-01-10 15:14:06 +0200

    add coords for station

--- kafka_producer.py
+++ kafka_producer.py
@@ -8,10 +8,11 @@ from datetime import datetime
 
 # Finland Traffic Measurements API
 API_URL = "https://tie.digitraffic.fi/api/tms/v1/stations/data"
-
+API_COORDINATE_URL = "https://tie.digitraffic.fi/api/tms/v1/stations"
 
 # Kafka configuration
 KAFKA_TOPIC = "traffic-finland"
+KAFKA_TOPIC_STATION = "traffic-finland-station"
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 
 producer = KafkaProducer(
@@ -21,6 +22,11 @@ producer = KafkaProducer(
 
 def fetch_traffic_data():
     response = requests.get(API_URL)
+    response.raise_for_status()
+    return response.json()
+
+def fetch_station_metadata():
+    response = requests.get(API_COORDINATE_URL)
     response.raise_for_status()
     return response.json()
 
@@ -49,14 +55,52 @@ def produce_events(data):
                     value=event
                 )
 
-                print(event)
+                #print(event)
 
             except Exception as e:
                 print(f"Skipping sensor at station {station_id}: {e}")
 
     producer.flush()
 
+def produce_station_metadata(data):
+    for station in data.get("features", []):
+
+        #pprint.pprint(station)
+        try:
+            station_id = station.get("id")
+            geometry  = station.get("geometry")
+            coordinates = geometry.get("coordinates")
+
+            event = {
+                "station_id": station.get("id"),  # unique station identifier
+                "longitude": station.get("geometry", {}).get("coordinates", [None, None, 0.0])[0],
+                "latitude": station.get("geometry", {}).get("coordinates", [None, None, 0.0])[1],
+                "altitude": station.get("geometry", {}).get("coordinates", [None, None, 0.0])[2],
+            }
+
+            print(event)
+
+            producer.send(
+                KAFKA_TOPIC_STATION,
+                key=str(station.get("stationId")).encode("utf-8"),
+                value=event
+            )
+
+        except Exception as e:
+            print(f"Skipping station {station.get('stationId')}: {e}")
+
+    producer.flush()
+
 def main():
+
+    try:
+        station_metadata = fetch_station_metadata()
+        print("Fetched station metadata from API")
+        produce_station_metadata(station_metadata)
+        print("Station metadata sent to Kafka")
+    except Exception as e:
+        print(f"Error fetching or producing station metadata: {e}")
+
     while True:
         try:
             data = fetch_traffic_data()
